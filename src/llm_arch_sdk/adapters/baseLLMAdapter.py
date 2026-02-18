d12535373d6902ad5fd45659338925313c7eac73
Victor Espiritu <espiritusantiagovictor@gmail.com>
2026-02-17 23:39:16 -0500

    refactor: actualizar baseLLMAdapter

--- src/llm_arch_sdk/adapters/baseLLMAdapter.py
+++ src/llm_arch_sdk/adapters/baseLLMAdapter.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from llm_arch_sdk.models.chat_completion import ChatCompletionResult
+
 
 class BaseLLMAdapter(ABC):
 
@@ -14,4 +16,47 @@ class BaseLLMAdapter(ABC):
         agent_id: str | None = None,
         **kwargs
     ) -> ChatCompletionResult:
-        ...
+        pass
+    
+    @abstractmethod
+    def completion(
+        self,
+        *,
+        model: str,
+        messages: list,
+        session_id: str | None = None,
+        job_id: str | None = None,
+        agent_id: str | None = None,
+        **kwargs
+    ) -> ChatCompletionResult:
+        pass
+    
+    @abstractmethod
+    def embed(
+        self,
+        *,
+        model: str,
+        input: list[str],
+        session_id: str | None = None,
+        job_id: str | None = None,
+        agent_id: str | None = None,
+        **kwargs
+    ) -> ChatCompletionResult:
+        pass
+    
+    
+    @abstractmethod
+    def health(
+        self,
+        *,
+        model: str,
+        input: list[str],
+        session_id: str | None = None,
+        job_id: str | None = None,
+        agent_id: str | None = None,
+        **kwargs
+    ) -> ChatCompletionResult:
+        pass
+    
+        
+    
